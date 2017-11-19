7b81909ce0cc4d76785c85bb083c5f669cc87cd5
AlejandraCP <alejandracp710@gmail.com>
2017-11-19 11:47:30 -0500

    Agregando versión 0.0.2 y versión 0.0.3

--- js/app.js
+++ js/app.js
@@ -4,6 +4,10 @@ var lista = document.getElementById('container-text');
 var counter = document.getElementById('counter');
 
 button.addEventListener('click', tweet);
+boxText.addEventListener('keyup', countChar);
+boxText.addEventListener('keyup', buttonAble);
+
+var numChar = parseInt(counter.dataset.number);
 
 function tweet(event) {
     if (boxText.value) {
@@ -16,5 +20,39 @@ function tweet(event) {
         parragr.appendChild(span);
         boxText.value = '';
         span.textContent = ' ' + time();
+    }
+}
+
+function countChar() {
+    counter.textContent = numChar;
+    numChar = 139 - boxText.textLength;
+    if (numChar < 0) {
+        button.removeEventListener('click', tweet);
+        counter.classList.add('charRed');
+        counter.classList.remove('char130');
+        counter.classList.remove('char120');
+    } else if (numChar < 10) {
+        button.addEventListener('click', tweet);
+        counter.classList.add('char120');
+        counter.classList.remove('charRed');
+        counter.classList.remove('char130');
+    } else if (numChar < 20) {
+        button.addEventListener('click', tweet);
+        counter.classList.add('char130');
+        counter.classList.remove('char120');
+    } else if (numChar >= 0) {
+        button.addEventListener('click', tweet);
+    } else if (numChar >= 0 || numChar <= 120) {
+        counter.classList.add('charBlack');
+        counter.classList.remove('char130');
+        counter.classList.remove('char120');
+    }
+}
+
+function buttonAble() {
+    if (numChar < 0) {
+        button.classList.add('buttonDisable');
+    } else if (numChar >= 0) {
+        button.classList.remove('buttonDisable');
     }
 }
