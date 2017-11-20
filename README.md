240c09fa9611794b2e8319f18eb7b09f8003ac5f
AlejandraCP <alejandracp710@gmail.com>
2017-11-20 02:03:56 -0500

    Agreando cambios y avance finl a index.html / app.js / main.css / README.md

---
+++ README.md
@@ -0,0 +1,45 @@
+# Replicar el newsfeed de Twitter
+
+## Objetivo
+El reto consistió en replicar el  **newsfeed** de Twitter, para ellos se siguió un flujo de versiones para ir habilitando poco a poco la funcionalidad de la página.
+
+
+## Versiones Solicitadas
+
+### Versión 0.0.1
+
+* Diseñar un formulario que permita ingresar un texto y un botón para "twittear".
+* Agregar un evento de click al botón o de submit al formulario.
+* En el evento, obtener el texto.
+* Agregar el texto al HTML.
+### Versión 0.0.2
+
+* No ingresar texto vacío (deshabilitar el botón de "twittear").
+* Contar la cantidad de caracteres de forma regresiva.
+### Versión 0.0.3
+
+* Si pasa los 140 caracteres, deshabilitar el botón.
+* Si pasa los 120 caracteres, mostrar el contador con OTRO color.
+* Si pasa los 130 caracteres, mostrar el contador con OTRO color.
+* Si pasa los 140 caracteres, mostrar el contador en negativo.
+### Versión 0.0.4
+
+* Al presionar enter que crezca el textarea de acuerdo al tamaño del texto.
+### Versión 0.0.5 (Extra)
+
+* Si la cantidad de caracteres ingresados (sin dar un enter), supera al tamaño del textarea por defecto, debe de agregarse una línea más para que no aparezca el scroll. (Si en caso aplica)
+### Versión 0.0.6 (Extra)
+
+* Agregar la hora en que se publicó el tweet. En el formato de 24 horas: hh:mm.
+
+## Archivos adjuntos en repositorio
+
+* Archivo **index.html** contiene estructura de la página web.
+
+* Carpeta **css** contiene el archivo **main.css** donde se han agragado los  estilos necesarios para el proyecto.
+
+* Carpeta **assets** se encuentran las imágenes necesarias para el proyecto.  
+
+* Carpeta **js** contiene el archivo **app.js**, se encuentra código JavaScript, el cuál da funcionalidad a la página.
+
+* Los íconos se obtuvieron de  Fontawesome.
--- js/app.js
+++ js/app.js
@@ -1,75 +1,78 @@
 var boxText = document.getElementById('insert-text');
 var button = document.getElementById('save');
-var lista = document.getElementById('container-text');
 var counter = document.getElementById('counter');
 
 button.addEventListener('click', tweet);
+boxText.addEventListener('keydown' , areaSize)
 boxText.addEventListener('keyup', countChar);
-boxText.addEventListener('keyup', addRow);
-boxText.addEventListener('keyup', buttonAble);
 
-var numChar = parseInt(counter.dataset.number);
 
 function tweet(event) {
-    if (boxText.value) {
-        var li = document.createElement('li');
-        var parragr = document.createElement('p');
-        var span = document.createElement('span');
-        parragr.textContent = boxText.value;
-        li.appendChild(parragr);
-        lista.appendChild(li);
-        parragr.appendChild(span);
+    if (boxText.value && boxText.value !== ' ') {
+        var textTweet = boxText.value;
+        var doTweet = document.createElement('div');
+        var containerTweet = document.querySelector('.container-tweet');
+        doTweet.textContent = textTweet + ' ' + time();
+        containerTweet.appendChild(doTweet);
         boxText.value = '';
-        span.textContent = ' ' + time();
+        counter.textContent = 140;
+        doTweet.classList.add('tweet-style');
     }
 }
 
-function countChar() {
-    counter.textContent = numChar;
-    numChar = 139 - boxText.textLength;
-    if (numChar < 0) {
-        button.removeEventListener('click', tweet);
-        counter.classList.add('charRed');
-        counter.classList.remove('char130');
-        counter.classList.remove('char120');
-    } else if (numChar < 10) {
-        button.addEventListener('click', tweet);
-        counter.classList.add('char120');
-        counter.classList.remove('charRed');
-        counter.classList.remove('char130');
-    } else if (numChar < 20) {
-        button.addEventListener('click', tweet);
-        counter.classList.add('char130');
-        counter.classList.remove('char120');
-    } else if (numChar >= 0) {
-        button.addEventListener('click', tweet);
-    } else if (numChar >= 0 || numChar <= 120) {
-        counter.classList.add('charBlack');
-        counter.classList.remove('char130');
-        counter.classList.remove('char120');
-    }
-}
-
-function buttonAble() {
-    if (numChar < 0) {
-        button.classList.add('buttonDisable');
-    } else if (numChar >= 0) {
-        button.classList.remove('buttonDisable');
-    }
-}
-
-function addRow(event) {
+function areaSize(event){
     var numberColum = boxText.getAttribute('cols');
     var numberRow = boxText.getAttribute('rows');
     if (boxText.value.length !== 0) {
         if (event.keyCode == 13 || parseInt(boxText.value.length) % 80 == 0) {
             var rowsFinal = parseInt(numberRow) + 1;
             boxText.setAttribute('rows', rowsFinal);
+        } else if (event.keyCode == 8) {
+            rowsFinal = parseInt(numberRow) - 1;
+            boxText.setAttribute('rows', rowsFinal);
         }
-    } else {
-        boxText.setAttribute('rows', 2);
+    } else if (boxText.value.length == 0) {
+        rowsFinal = 2;
+        boxText.setAttribute('rows', rowsFinal);
     }
 }
+
+function countChar() {
+    var numChar = boxText.value.length;
+    var showNumberChar = 140 - numChar;
+    counter.textContent = showNumberChar;
+    if (numChar > 0 && numChar < 119 ) {
+        counter.classList.add('char-black');
+        counter.classList.remove('char130');
+        counter.classList.remove('char120');
+        counter.classList.remove('char-red');
+        button.addEventListener('click', tweet);
+    } else if (numChar >= 120 && numChar <= 130) {
+        counter.classList.add('char120');
+        counter.classList.remove('char130');
+        counter.classList.remove('char-black');
+        counter.classList.remove('char-red');
+        button.addEventListener('click', tweet);
+    } else if (numChar > 130 && numChar <= 140) {
+        counter.classList.add('char130');
+        counter.classList.remove('char120');
+        counter.classList.remove('char-black');
+        counter.classList.remove('char-red');
+        button.addEventListener('click', tweet);
+    } else if (numChar > 140) {
+        button.removeEventListener('click', tweet);
+    } 
+}
+
+
+// function buttonAble() {
+//     if (numChar < 0) {
+//         button.classList.add('buttonDisable');
+//     } else if (numChar >= 0) {
+//         button.classList.remove('buttonDisable');
+//     }
+// }
+
 
 function time() {
     var actualDate = new Date();
